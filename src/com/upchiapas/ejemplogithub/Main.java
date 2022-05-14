a4e6173228d710137a29b81d43dccfcba9819f48
alilopez <alzunun@upchiapas.edu.mx>
2022-05-14 05:50:00 -0500

    Actualización de Main

--- src/com/upchiapas/ejemplogithub/Main.java
+++ src/com/upchiapas/ejemplogithub/Main.java
@@ -3,5 +3,7 @@ package com.upchiapas.ejemplogithub;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+
+        System.out.println("Bienvenido a la clase de POO");
     }
 }
